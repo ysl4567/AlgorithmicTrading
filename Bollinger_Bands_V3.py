2d49a96d0bcbb8d920727bae4b018d7e0a7b247b
ysl4567 <117042067+ysl4567@users.noreply.github.com>
2022-11-01 20:56:52 -0700

    Update Bollinger_Bands_V3.py

--- Bollinger_Bands_V3.py
+++ Bollinger_Bands_V3.py
@@ -14,7 +14,7 @@ import datapane as dp
 #Lower Band = Middle Band - (2 * 20 standard deviation of close price)
 
 #ticker input
-#for input, change 'VOO' to ticker_input and vice versa
+#for input, change 'VOO' to ticker_input
 ticker_input = input("Please enter ticker value (w/o $ symbol): ")
 
 #VOO Graph for 
@@ -22,8 +22,8 @@ voo = yf.Ticker(ticker_input)
 print(voo.history(period="18mo"))
 
 #Graph VOO Closing price for the past 18 months
-voo = yf.download(ticker_input , period='18mo', Interval='1d') #print everyday on the graph - plotly.express
-voo_price_chart = px.line(voo['Close'],title= ticker_input + ' Daily Close Price',color_discrete_map={'Close':'gray'},width=1000, height=1000)
+voo = yf.download(ticker_input, period='18mo', Interval='1d') #print everyday on the graph - plotly.express
+voo_price_chart = px.line(voo['Close'],title= 'VOO' + ' Daily Close Price',color_discrete_map={'Close':'gray'},width=1000, height=1000)
 voo_price_chart.show()
 
 #Middle Band - 20 Day Simple Moving Average
@@ -39,38 +39,86 @@ voo['UPPER'] = voo['MID'] + (2*(voo['STDDEV']))
 #Lower Band - Middle Band - (2 * 20 standard deviation of close price)
 voo['LOWER'] = voo['MID'] - (2*(voo['STDDEV']))
 
-#where function, where inequality is true print true, else print false 
-voo['SELL'] = np.where(voo['UPPER'] < voo['Close'], True, False)
-voo['BUY'] = np.where(voo['LOWER'] > voo['Close'], True, False)
-print (voo)
 
-#graph when to buy and sell
+#When to buy and sell - https://www.fidelity.com/viewpoints/active-investor/understanding-bollinger-bands#:~:text=Buy%20and%20sell%20signals,-In%20addition%20to&text=When%20it%20breaks%20below%20the,but%20rather%20a%20continuation%20pattern.
+#Buy - When the closing prices goes above the upper band
+#Sell - When the closing price goes below the lower band 
+#where function - where inequality is true print value, else false don't print value
+sell_value_dates = []
+buy_value_dates = []
+buy_value_close = []
+sell_value_close = []
+for i in range (len(voo)): #all possible sell dates counting from start date
+    if voo['UPPER'][i] < voo['Close'][i]:
+        sell_value_dates.append(i)
+print (sell_value_dates)
+
+for j in range (len(voo)): #all possible buy dates counting from start date
+    if voo['LOWER'][j] > voo['Close'][j]:
+        buy_value_dates.append(j)
+print (buy_value_dates)
+
+for k in range (len(voo)): #all the UPPER close values
+    if voo['UPPER'][k] < voo['Close'][k]:
+        sell_value_close.append(voo['UPPER'][k])
+print (sell_value_close)
+
+for l in range (len(voo)): #all the LOWER close values
+    if voo['LOWER'][l] > voo['Close'][l]:
+        buy_value_close.append(voo['LOWER'][l])
+print (buy_value_close)
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 sell_point = []
 buy_point = []
-minimum_length = min(len(sell_point),len(buy_point))
-x = -1
+buy_point_2 = []
+sell_point_2 = []
+minimum_length = min(len(sell_value_dates),len(buy_value_dates),len(sell_value_close), len(buy_value_close))
+x = 0
+y = x
 bought = False
-#set up for loop to check for all avlues in voo range.
-for dates in range(minimum_length):
-    if voo['LOWER'][dates] > voo['Close'][dates]: #buy
-        if bought == False:
-            buy_point.append(dates)
-            bought = True
-        elif voo['UPPER'][dates] < voo['Close'][dates] and bought == True: #sell
-            if bought == True:
-                sell_point.append(dates)
-                bought = False
-            
-#Graph with Matplotlib 
+for x in range(minimum_length):
+    for y in range (y,y+2):
+        if sell_value_dates[x] > buy_value_dates[x] and sell_value_close[x] > buy_value_close[x]:
+            if bought == False:
+                buy_point_2.append(buy_value_dates[x])
+                sell_point_2.append(sell_value_dates[x])
+                sell_point.append(sell_value_close[x])
+                buy_point.append(buy_value_close[x])
+                bought == True
+
+#Graph with Matplotlib - select
 #plt.plot(voo[['Close', 'MID', 'UPPER', 'LOWER']])
-voo[['Close', 'MID', 'UPPER', 'LOWER']].plot(label =  ticker_input, figsize=(20,10))
-plt.fill_between(voo.index, voo.UPPER, voo.LOWER, color = 'grey', alpha = 0.3)
-plt.scatter(voo.index[voo.BUY], voo[voo.BUY].Close, marker = '^', color = 'green') #buy -all
-plt.scatter(voo.index[voo.SELL], voo[voo.SELL].Close, marker = '^', color = 'red') #buy -all
-#plt.scatter(voo.iloc[buy_point].index, voo.iloc[buy_point].Close, marker = '^', color = 'green') #buy - select
-#plt.scatter(voo.iloc[sell_point].index, voo.iloc[sell_point].Close, marker = '^', color = 'red') #sell - select
+voo[['Close', 'MID', 'UPPER', 'LOWER']].plot(label = ticker_input, figsize=(20,10))
+plt.fill_between(voo.index, voo.UPPER, voo.LOWER, color = 'grey', alpha=0.3)
+plt.scatter(voo.iloc[buy_point_2].index, voo.iloc[buy_point_2].Close, marker = '^', color = 'green') #buy     -- Date Since start, Close Value 
+#INDEX OF BUY_POINT_2 AND BUY_POINT MUST BE THE SAME
+plt.scatter(voo.iloc[sell_point_2].index, voo.iloc[sell_point_2].Close, marker = '^', color = 'red') #sell
+#plt.scatter(voo.index[voo.BUY], voo[voo.BUY].Close, marker = '^', color = 'green') #buy
+#plt.scatter(voo.index[voo.SELL], voo[voo.SELL].Close, marker = '^', color = 'red') #sell
 plt.show()
 
 
-all_buy_and_sell = pd.concat([voo[voo.BUY].Close, voo[voo.SELL].Close], axis = 1)
-print (all_buy_and_sell)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
