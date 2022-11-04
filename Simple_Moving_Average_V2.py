d0758dc8c5538b44a1f207997c525cff8a199e49
ysl4567 <117042067+ysl4567@users.noreply.github.com>
2022-11-04 05:07:12 -0700

    Update Simple_Moving_Average_V2.py

--- Simple_Moving_Average_V2.py
+++ Simple_Moving_Average_V2.py
@@ -17,19 +17,19 @@ ticker_input = input("Enter ticker without the $ symbol: ")
 
 #input any stock ticker
 alb = yf.Ticker(ticker_input)
-print(alb.history(period="24mo"))
+print(alb.history(period="36mo"))
 
 #Graph ALB Closing price for the past 18 months
-alb = yf.download(ticker_input, period='24mo', Interval='1d') #print everyday on the graph 
+alb = yf.download(ticker_input, period='36mo', Interval='1d') #print everyday on the graph 
 alb_price_chart = px.line(alb['Close'],title='Albemarle Daily Close Price',color_discrete_map={'Close':'gray'},width=1000, height=1000)
 alb_price_chart.show()
 
 #In project 1, we want to explore Moving Averages Algorithmic Trading
-#Short Moving Average. Set this equal to 5 days. Using close value.
-alb['ALB20'] = alb['Close'].rolling(20).mean()
+#Short Moving Average. Set this equal to 50 days. Using close value.
+alb['20'] = alb['Close'].rolling(20).mean()
 alb['y'] = 70
-#Long Moving Average. Set this equal to 100 days. Uinsg close value
-alb['ALB50'] = alb['Close'].rolling(50).mean()
+#Long Moving Average. Set this equal to 200 days. Uinsg close value
+alb['50'] = alb['Close'].rolling(50).mean()
 print (alb)
 
 buy_list = []
@@ -38,11 +38,11 @@ bought = False
 
 
 for dates in range(len(alb)):
-    if bought == False and alb['ALB20'][dates] > alb['ALB50'][dates]:
+    if bought == False and alb['20'][dates] > alb['50'][dates]:
         if bought == False:
             buy_list.append(dates)
             bought = True
-    elif bought == True and alb['ALB20'][dates] < alb['ALB50'][dates]:
+    elif bought == True and alb['20'][dates] < alb['50'][dates]:
         if bought == True:
             sell_list.append(dates)
             bought = False
@@ -141,7 +141,7 @@ print ("=",alb.iloc[length_sell_list_new].Close)
 
 #Print on graph
 #Short Moving Average - better for day trading. Accounts for abnormalities. Long Moving Average - Better for long term. Does not account for abnormalities
-alb[['Close','ALB20', 'ALB50']].plot(label = ticker_input, figsize=(20,10))
+alb[['Close','20', '50']].plot(label = ticker_input, figsize=(20,10))
 #alb[['Close','ALB50']].plot(label = 'ALB', figsize=(20,10))
 #alb[['Close','ALB100']].plot(label = 'ALB', figsize=(20,10))
 plt.scatter(alb.iloc[length_buy_date_new].index, alb.iloc[length_buy_date_new].Close, marker = '^', color = 'green') #buy - select
